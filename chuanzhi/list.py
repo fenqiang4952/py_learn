ddf004fadd604792087ffe24c3ddb868a4f133f5
fenqiang4952 <495246490@qq.com>
2018-10-08 11:19:58 +0800

    updated

---
+++ chuanzhi/list.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+A = ['xiaoWang','xiaoZhang','xiaoHua']
+
+A.append('add')
+
+print(A)
---
+++ chuanzhi/string.py
@@ -0,0 +1,8 @@
+name = 'abcdef'
+
+print(name[0])
+print(name[1])
+print(name[2])
+
+print(name[0:10:3])
+print(name[::-1])
